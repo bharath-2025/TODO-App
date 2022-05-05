048993bba6087734f3dd548c9dad4bc59208ac3d
Bharath2025 <mbharath@karunya.edu.in>
2022-05-05 16:38:39 +0530

    Setting up the body parser to read data from the form using req.body and static files setup is done.

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,31 @@
+var todo_list =[
+    {
+        desc:'buying vegetables',
+        category:'personal',
+        due:'2/5/22'
+    },
+    {
+        desc:'Complete Mini project',
+        category:'school',
+        due:'10/5/22'
+    },
+    {
+        desc:'perepare for placement',
+        category:'Work',
+        due:'28/5/22'
+    }
+]
+
 module.exports.home = function(req,res){
    return res.render('home',{
        title:'TODO APP',
-       body: 'This is the body part of home page'
+       body: 'This is the body part of home page',
+       todoList: todo_list
    })
+}
+
+module.exports.createTodo = function(req,res){
+    //console.log(req.body);
+    todo_list.push(req.body);
+    return res.redirect('back');
 }
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const { urlencoded } = require('body-parser');
 const express = require('express');
 const port = 5000;
 const app = express();
+
+// Setting up the body-parser to read or decode the form data
+// No need to install install body-parser it is already available in express just use it as a middleware
+app.use(express.urlencoded());
 
 // MiddleWare to use the routes inside routes folder.
 app.use('/',require('./routes/index'));
@@ -10,7 +15,8 @@ app.use('/',require('./routes/index'));
 app.set('view engine','ejs');   // telling to our express server to use/setup EJS as the default template engine.
 app.set('views','./views');     // setting up the path for views.
 
-
+// setting up the static files using middleware
+app.use(express.static('./assets'));
 
 
 app.listen(port,function(err){
